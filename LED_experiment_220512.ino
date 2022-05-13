3775f5c01c59629aa9886411184a39c2e2f300e6
김하경 <khg05311@naver.com>
2022-05-13 10:26:28 +0900

    LED_blinking_by_the_distance betweeen obejct and camera

--- LED_experiment_220512.ino
+++ LED_experiment_220512.ino
@@ -6,31 +6,36 @@
 #define Sig_1 1
 #define Sig_2 4
 
-#define Size 100 
+#define Size_1 40
+#define Size_2 30
+
 Pixy2 pixy;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial.print("Starting...\n");
 
   pixy.init();
 
-  pinMode ( LED_R, OUTPUT);  // to set the OUTPUT mode of pin number 13.  
-  pinMode ( LED_G, OUTPUT);  // to set the OUTPUT mode of pin number 7.  
+  pinMode(LED_R, OUTPUT); // to set the OUTPUT mode of pin number 13.
+  pinMode(LED_G, OUTPUT); // to set the OUTPUT mode of pin number 7.
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 
   int i;
   pixy.ccc.getBlocks();
 
-  if(pixy.ccc.numBlocks) {
+  if (pixy.ccc.numBlocks)
+  {
     Serial.print("Object detected ");
     Serial.print(pixy.ccc.numBlocks);
     Serial.print("\n");
 
-    for (i=0; i<pixy.ccc.numBlocks; i++)
+    for (i = 0; i < pixy.ccc.numBlocks; i++)
     {
       Serial.print("  block");
       Serial.print(i);
@@ -39,22 +44,60 @@ void loop() {
     }
 
     // 사물이 계속 인식될 때 LED를 초기화 해주는 단
-    digitalWrite (LED_R, LOW); 
-    digitalWrite (LED_G, LOW); 
-    
-    for (i=0; i<pixy.ccc.numBlocks; i++)
+    digitalWrite(LED_R, LOW);
+    digitalWrite(LED_G, LOW);
+
+    for (i = 0; i < pixy.ccc.numBlocks; i++)
     {
-      if ( pixy.ccc.blocks[i].m_signature == Sig_1) {
-        //if the object's width value is greater than SIZE blink faster
-      digitalWrite (LED_G, HIGH);
+      if (pixy.ccc.blocks[i].m_signature == Sig_1)
+      {
+        // if the object_1's width value is greater than Size_1, LED_G blink faster
+        if (pixy.ccc.blocks[i].m_width > Size_1)
+        {
+          digitalWrite(LED_G, HIGH);
+          delay(100);
+          digitalWrite(LED_G, LOW);
+          delay(100);
+          digitalWrite(LED_G, HIGH);
+          delay(100);
+          digitalWrite(LED_G, LOW);
+          delay(100);
+        }
+        else
+        {
+          digitalWrite(LED_G, HIGH);
+          delay(200);
+          digitalWrite(LED_G, LOW);
+          delay(200);
+        }
       }
-     else if ( pixy.ccc.blocks[i].m_signature == Sig_2) {
-      digitalWrite (LED_R, HIGH);
+      else if (pixy.ccc.blocks[i].m_signature == Sig_2)
+      {
+        // if the object_2's width value is greater than Size_2, LED_R blink faster
+        if (pixy.ccc.blocks[i].m_width > Size_2)
+        {
+          digitalWrite(LED_R, HIGH);
+          delay(100);
+          digitalWrite(LED_R, LOW);
+          delay(100);
+          digitalWrite(LED_R, HIGH);
+          delay(100);
+          digitalWrite(LED_R, LOW);
+          delay(100);
+        }
+        else
+        {
+          digitalWrite(LED_R, HIGH);
+          delay(200);
+          digitalWrite(LED_R, LOW);
+          delay(200);
+        }
       }
     }
   }
-  else {
-    digitalWrite (LED_R, LOW); 
-    digitalWrite (LED_G, LOW); 
+  else
+  {
+    digitalWrite(LED_R, LOW);
+    digitalWrite(LED_G, LOW);
   }
 }
